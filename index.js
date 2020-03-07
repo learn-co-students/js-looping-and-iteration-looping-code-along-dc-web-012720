41016d9d0478b232282c3c94f7a4272527d67d2e
JasonP77 <llvslr77@gmail.com>
2020-03-07 14:53:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let name = ["Ada", "Brendan", "Ali"]
+let day = "birthday";
+
+function writeCards(name, day){
+	let arr = []
+	for (let i = 0; i < name.length; i++){
+		arr.push(`Thank you, ${name[i]}, for the wonderful ${day} gift!`)
+	}
+	return arr
+}
+
+
+let number = 10
+function countDown(number){
+	while(number > 0 ){
+		console.log(number);
+		number -= 1;
+	}
+	console.log(number);
+}
